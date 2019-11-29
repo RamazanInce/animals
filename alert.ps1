596498b660fe34cf5f8e573a26a7410643b0d0be
RamazanInce <45245705+RamazanInce@users.noreply.github.com>
2019-11-29 11:42:16 +0100

    Update alert.ps1

--- alert.ps1
+++ alert.ps1
@@ -1,9 +1,9 @@
-Foreach ($alert in $sub2_alerts)
+Foreach ($alert in $sub1_alerts)
 {
-If ($datetimecheckCriticals -gt $alert.TimeRaised -and [System.String]::IsNullOrEmpty($alert.TicketId)-AND (-not$AllAlertIds.Contains($([string]$alert.Id))))
+If ($datetimecheckCriticals -gt $alert.TimeRaised -and [System.String]::IsNullOrEmpty($alert.TicketId) -AND (-not$AllAlertIds.Contains($([string]$alert.Id))))
 {
-$AllAlertIds+=$([string]$alert.Id) # add to keep track of it.
-write-output "Subscription 2 Alerts"
+$AllAlertIds+=$([string]$alert.Id)# add to keep track of it.
+write-output "Subscription 1 Alerts"
 Write-Output "alertid<>$([string]$alert.Id)###"
 Write-Output "alertname<>$($alert.Name)###"
 If ([system.string]::IsNullOrEmpty($alert.NetbiosComputerName))
@@ -25,11 +25,11 @@ Write-Output "alertMMmodeLastModified<>$([System.TimeZoneInfo]::ConvertTimeFromU
 try{
 [string]$extra=([xml]$alert.Context | Select-xml -Xpath "//EventDescription" -ErrorAction Stop | Select-Object -ExpandProperty "node").'#cdata-section'
  $extra = "`n" + $extra
- Write-Output "alertDescription<>$($alert.Description)$($extra)###"
+Write-Output "alertDescription<>$($alert.Description) $($extra)###"
 }
 catch
 {
 }
- Write-Output "|||" 
+Write-Output "|||" 
 }
 }
